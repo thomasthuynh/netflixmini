8a80c266a7ccad1a61a1524ab58e4f4ba9ce80ca
ThomasHuynh <Thomas_TTH@live.com>
2023-01-19 23:33:54 -0500

    implemented movie search bar

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import MovieContainer from './MovieContainer';
 function App() {
 
   const [movieData, setMovieData] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState();
+  const [selectedMovie, setSelectedMovie] = useState({});
+  const [searchValue, setSearchValue] = useState("");
 
-  const fetchMovies = async () => {
-    const response = await axios.get("https://api.themoviedb.org/3/movie/now_playing",
+  const fetchMovies = async (searchValue) => {
+    const lookupType = searchValue ? "search" : "discover";
+    const response = await axios.get(`https://api.themoviedb.org/3/${lookupType}/movie/`,
     {
       params: {
-        api_key: "02a015f767f49fbd46124014022d6a5c"
+        api_key: "02a015f767f49fbd46124014022d6a5c",
+        query: searchValue
       }
     })
 
@@ -25,17 +28,29 @@ function App() {
     fetchMovies();
   }, [])
 
-console.log(movieData);
+
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchValue);
+  }
+
 
   return (
     <div className="App">
 
     <header>
       <h1>Miniflix</h1>
+
+      <form onSubmit={searchMovies}>
+        <input type="text" onChange={e => setSearchValue(e.target.value)}/>
+        <button>Search</button>
+      </form>
+
+      <p>{searchValue}</p>
     </header>
 
     <main>
-      <section className="movieContent" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${selectedMovie.backdrop_path})`}}>
+      <section className="movieContent" style={selectedMovie.backdrop_path ? {backgroundImage: `url(https://image.tmdb.org/t/p/original${selectedMovie.backdrop_path})`} : null}>
         <div className="movieDetails">
           <h2>{selectedMovie.title}</h2>
           <p>{selectedMovie.overview}</p>
