8d6f0a08581830d90dd5c8a355d82e3c1bf5e229
ThomasHuynh <Thomas_TTH@live.com>
2023-04-03 16:47:55 -0400

    added searchValue as dependency in Home component useEffect

--- src/components/Home.js
+++ src/components/Home.js
@@ -142,7 +142,7 @@ const Home = () => {
   // This useEffect will run the fetchMovies function on page load, displaying the top twenty trending movies
   useEffect(() => {
     fetchMovies(searchValue)
-  }, [])
+  }, [searchValue])
 
   return (
     <div className="App">
